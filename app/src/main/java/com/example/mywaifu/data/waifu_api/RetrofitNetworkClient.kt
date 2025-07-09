cca7913fa05c1373680bdb9c034ffc70b61691d4
popka <drensymax@gmail.com>
2025-07-09 09:34:39 +0300

    Чистим: мелкие правки верстки и обработка ошибок в RetrofitNetworkClient
    - RetrofitNetworkClient.kt добавили блок try\catch чтобы ловить исключение если нет подключения
    
    -

--- app/src/main/java/com/example/mywaifu/data/waifu_api/RetrofitNetworkClient.kt
+++ app/src/main/java/com/example/mywaifu/data/waifu_api/RetrofitNetworkClient.kt
@@ -5,8 +5,9 @@ import com.example.mywaifu.data.waifu_api.models.Response
 import com.example.mywaifu.data.waifu_api.models.WaifuRequest
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.IOException
 
-class RetrofitNetworkClient() : NetworkClient {
+class RetrofitNetworkClient: NetworkClient {
 
     val retrofit = Retrofit.Builder()
         .baseUrl("https://api.waifu.pics/")
@@ -20,11 +21,16 @@ class RetrofitNetworkClient() : NetworkClient {
 
         if (dto is WaifuRequest) {
 
-            val call = waifuService.getSingleImage(dto.type, dto.category)
-            val resp = call.execute()
-            val body = resp.body() ?: Response()
+            return try {
+                val call = waifuService.getSingleImage(dto.type, dto.category)
+                val resp = call.execute()
+                val body = resp.body() ?: Response()
 
-            return body.apply { resultCode = resp.code() }
+                body.apply { resultCode = resp.code() }
+            } catch (e: IOException) {
+                Response().apply { resultCode = -1 }
+            }
+
         } else {
             return Response().apply { resultCode = 400 }
         }
